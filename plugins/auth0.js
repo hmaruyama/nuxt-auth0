84d36a6e1938856629bc04e025a18c11ff8c6cae
Hikaru Maruyama <7115497+hmaruyama@users.noreply.github.com>
2019-02-09 23:44:45 +0900

    add JWT

--- plugins/auth0.js
+++ plugins/auth0.js
@@ -1,4 +1,6 @@
 import Auth0Lock from 'auth0-lock'
+import jwtDecode from 'jwt-decode'
+import queryString from 'query-string'
 import nuxtConfig from '~/nuxt.config'
 const config = nuxtConfig.auth0
 
@@ -21,10 +23,24 @@ class Auth0Util {
     )
     lock.show()
   }
+  getQueryParams() {
+    return queryString.parse(location.hash)
+  }
+  setToken({access_token, id_token, expires_in}) {
+    const localStorage = window.localStorage
+    localStorage.setItem('accessToken', access_token)
+    localStorage.setItem('idToken', id_token)
+    localStorage.setItem('expiresAt', expires_in * 1000 + new Date().getTime())
+    localStorage.setItem('user', JSON.stringify(jwtDecode(id_token)))
+  }
+  setTokenByQuery() {
+    this.setToken(this.getQueryParams());
+  }
   getBaseUrl() {
     return `${window.location.protocol}//${window.location.host}`
   }
 }
+
 export default (context, inject) => {
   inject('auth0', new Auth0Util)
 }
